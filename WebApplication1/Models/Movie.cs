0c4054d1de6dfab357596a3a020703e85b241e90
Shaun <scalagos0123@live.com>
2017-04-27 19:39:19 +0800

    Add project files.

---
+++ WebApplication1/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Movie Name")]
+        public string Movie_Name { get; set; }
+    }
+
+    public class MovieDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
